a4876bab55442a11d8d941cc8ccea5537ccbc39f
Evandro Gubert <evandrogubert@sistran.com.br>
2025-05-06 22:40:27 -0300

    remove unused ButtonComponent import from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ButtonComponent } from "./components/button/button.component";
 import { CardComponent } from "./components/card/card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonComponent, CardComponent],
+  imports: [CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
